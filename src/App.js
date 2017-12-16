76ebf73b7155f7d47dcfcb656a1d60de9f9d6e72
Ryan Lott <toranlyt@gmail.com>
2017-12-15 22:01:17 -0600

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import './App.css';
+
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
-import './App.css';
-import logo from './logo.svg';
+
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
   render() {
@@ -10,7 +32,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
